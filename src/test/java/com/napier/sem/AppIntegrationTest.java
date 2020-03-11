7d3e9c7e61205467d2909a288cc9bc158534ece3
Jan Pfeiffer <40457584@live.napier.ac.uk>
2020-03-11 13:55:37 +0000

    Travis update with jobs integration. Integration tests implemented.

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.napier.sem;
+
+import com.mysql.cj.jdbc.exceptions.ConnectionFeatureNotAvailableException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest
+{
+    static App app;
+    static ConnectionLayer connection;
+    static BusinessLayer businessLayer;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        connection = new ConnectionLayer();
+        connection.connect("localhost:33060");
+
+    }
+
+    @Test
+    void testGetEmployee()
+    {
+        Employee emp = businessLayer.getEmployee(255530, connection.con);
+        assertEquals(emp.emp_no, 255530);
+        assertEquals(emp.first_name, "Ronghao");
+        assertEquals(emp.last_name, "Garigliano");
+    }
+}
